467e467304143622f6e98cde382c2f01dd1db345
Nabila123 <nabilmunawar5@gmail.com>
2022-01-25 11:55:10 +0700

    Add Migration

---
+++ database/migrations/2022_01_25_035237_create_tr_gudang_masuk_detail_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrGudangMasukDetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tr_gudang_masuk_detail', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('gudangKeluarId');
+            $table->unsignedBigInteger('purchaseId');
+            $table->integer('qty')->nullable();
+            $table->timestamps();
+
+            
+            $table->foreign('gudangKeluarId')->references('id')->on('tr_gudang_keluar');
+            $table->foreign('purchaseId')->references('id')->on('tr_purchase');
+            $table->unique(['gudangKeluarId', 'purchaseId'], 'my_unique_ref');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tr_gudang_masuk_detail');
+    }
+}
