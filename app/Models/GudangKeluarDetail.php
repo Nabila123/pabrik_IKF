e97ad8a584e8f7d64767c6c7aa8e3b0c134ceda8
Nabila123 <nabilmunawar5@gmail.com>
2022-01-25 15:44:53 +0700

    Add Model Gudang Keluar & Masuk Detail

---
+++ app/Models/GudangKeluarDetail.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GudangKeluarDetail extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tr_gudang_keluar_detail';
+
+}
