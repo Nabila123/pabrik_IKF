6d388a8bd1e8335ffec8473fec964e0bdec7639d
linamunawwar <nuramalina2911.na@gmail.com>
2022-01-29 11:41:43 +0700

    edit migration

--- database/migrations/2022_01_29_040132_edit_table_tr_gudang_bahan_baku.php
+++ database/migrations/2022_01_29_040132_edit_table_tr_gudang_bahan_baku.php
@@ -15,10 +15,10 @@ class EditTableTrGudangBahanBaku extends Migration
     {
         Schema::table('tr_gudang_bahan_baku_detail', function (Blueprint $table) {
             $table->dropColumn('qty');
-            $table->integer('qty_permintaan');
-            $table->integer('qty_saat_ini')->nullable();
-            $table->integer('gramasi')->nullable();
-            $table->integer('diameter')->nullable();
+            $table->integer('qty_permintaan')->after('materialId');
+            $table->integer('qty_saat_ini')->after('qty_permintaan')->nullable();
+            $table->integer('gramasi')->after('qty_saat_ini')->nullable();
+            $table->integer('diameter')->after('gramasi')->nullable();
         });
 
         Schema::table('tr_gudang_bahan_baku', function (Blueprint $table) {
