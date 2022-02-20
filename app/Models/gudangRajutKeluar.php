69a9b8a7081377d411d70b417026626fb7bdc016
Nabila123 <nabilmunawar5@gmail.com>
2022-02-20 15:47:24 +0700

    Update Model Gudang Rajut

--- app/Models/gudangRajutKeluar.php
+++ app/Models/gudangRajutKeluar.php
@@ -10,33 +10,4 @@ class GudangRajutKeluar extends Model
     use HasFactory;
 
     protected $table = 'gd_rajutkeluar';
-
-    public function user()
-    {
-        return $this->hasOne('App\Models\User','id','userId');
-    }
-
-    public function gudang()
-    {
-        return $this->hasOne('App\Models\GudangBahanBaku','id','gudangId');
-    }
-
-    public function purchase()
-    {
-        return $this->hasOne('App\Models\AdminPurchase','id','purchaseId');
-    }
-
-    public function material()
-    {
-        return $this->hasOne('App\Models\MaterialModel','id','materialId');
-    }
-
-    public static function updateStatusDiterima($id, $statusDiterima)
-    {
-        $purchaseUpdated['statusDiterima'] = $statusDiterima;
-
-        self::where('id', $id)->update($purchaseUpdated);
-
-        return 1;
-    }
 }
--- app/Models/gudangRajutMasuk.php
+++ app/Models/gudangRajutMasuk.php
@@ -10,38 +10,4 @@ class GudangRajutMasuk extends Model
     use HasFactory;
 
     protected $table = 'gd_rajutmasuk';
-
-    public function user()
-    {
-        return $this->hasOne('App\Models\User','id','userId');
-    }
-
-    public function gudang()
-    {
-        return $this->hasOne('App\Models\GudangBahanBaku','id','gudangId');
-    }
-
-    public function purchase()
-    {
-        return $this->hasOne('App\Models\AdminPurchase','id','purchaseId');
-    }
-
-    public function material()
-    {
-        return $this->hasOne('App\Models\MaterialModel','id','materialId');
-    }
-
-    public function gudangKeluar()
-    {
-        return $this->hasOne('App\Models\GudangKeluar','id','gudangKeluarId');
-    }
-
-    public static function updateStatusDiterima($id, $gudangRequest)
-    {
-        $purchaseUpdated['statusDiterima'] = 1;
-
-        self::where('gudangRequest', $gudangRequest)->where('id', $id)->update($purchaseUpdated);
-
-        return 1;
-    }   
 }
